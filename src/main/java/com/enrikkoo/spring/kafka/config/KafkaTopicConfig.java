938f4b4cc3cb8d50bd7029923ef52cd34749fb17
Erik <enrikkoo@yandex.ru>
2022-05-03 16:16:41 +0300

    Producer with producerFactory

--- src/main/java/com/enrikkoo/spring/kafka/config/KafkaTopicConfig.java
+++ src/main/java/com/enrikkoo/spring/kafka/config/KafkaTopicConfig.java
@@ -1,19 +1,22 @@
 package com.enrikkoo.spring.kafka.config;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class KafkaTopicConfig {
 
-    @Bean
-    public NewTopic topic1() {
-        return TopicBuilder.name("thing1")
-                .partitions(2)
-                .replicas(3)
-                .compact()
-                .build();
-    }
+/*    @Bean
+    public KafkaAdmin admin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "pkc-l6wr6.europe-west2.gcp.confluent.cloud:9092");
+        return new KafkaAdmin(configs);
+    }*/
 }
--- src/main/java/com/enrikkoo/spring/kafka/producer/KafkaProducer.java
+++ src/main/java/com/enrikkoo/spring/kafka/producer/KafkaProducer.java
@@ -1,5 +1,42 @@
 package com.enrikkoo.spring.kafka.producer;
 
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.security.auth.SecurityProtocol;
+import org.apache.kafka.common.security.plain.PlainLoginModule;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class KafkaProducer {
+
+    @Value("${spring.kafka.properties.bootstrap.servers}")
+    private String BROKER_URL;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,BROKER_URL);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(SaslConfigs.SASL_MECHANISM,"PLAIN");
+        configProps.put(SaslConfigs.SASL_JAAS_CONFIG, String.format(
+                "%s required username=\"%s\" " + "password=\"%s\";", PlainLoginModule.class.getName(), "", ""
+        ));
+        configProps.put("security.protocol",SecurityProtocol.SASL_SSL.name());
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
